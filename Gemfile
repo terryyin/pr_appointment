eab7487741fbda1beae52b39ce63e2279e8123bb
Terry Yin <terry.yinzhe@gmail.com>
2017-07-25 11:02:26 +0800

    is there any?

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source "https://rubygems.org"
 
 # Specify your gem's dependencies in pr_appointment.gemspec
 gemspec
+gem 'capybara'
+gem 'selenium-webdriver'
--- spec/pr_appointment_spec.rb
+++ spec/pr_appointment_spec.rb
@@ -1,11 +1,36 @@
 require "spec_helper"
+require "selenium-webdriver"
 
-RSpec.describe PrAppointment do
-  it "has a version number" do
-    expect(PrAppointment::VERSION).not_to be nil
+Capybara.register_driver :selenium do |app|
+  Capybara::Selenium::Driver.new(
+    app,
+    browser: :firefox,
+    desired_capabilities: Selenium::WebDriver::Remote::Capabilities.firefox(marionette: false)
+  )
+end
+
+RSpec.describe PrAppointment, :type => :feature, :js => :true do
+
+  def available?
+    visit 'https://eappointment.ica.gov.sg/ibook/gethome.do'
+    select 'Apply for PR', from: 'apptDetails.apptType'
+    fill_in name: 'apptDetails.identifier1', with: ENV['MY_ID']
+    fill_in name: 'apptDetails.identifier2', with: '4'
+    fill_in name: 'apptDetails.identifier3', with: ENV['MY_NUMBER']
+    click_on 'Submit'
+    while not page.has_css?('.cal_AS') do
+      click_link '<Next>'
+      expect(page).to have_content ''
+      return false unless page.has_css?('.cal_AF')
+    end
+    return true
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "signs me in" do
+    while not available?
+      `say 'fully booked, try next time.'`
+      sleep 30 * 60
+    end
+    `say 'Hey, Good news. There is a timeslot available.'`
   end
 end
